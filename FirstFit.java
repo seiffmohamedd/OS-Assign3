2af19b105ea009fad16e3667ac6a6f2a240039e7
seiffmohamedd <seif500mohamed@gmail.com>
2022-12-15 10:51:38 +0200

    Last Update , in progress

--- FirstFit.java
+++ FirstFit.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 class FirstFit {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int NumOfPartitions, PartitionSize[], NumOfProcess, ProcessSize[], ans = 0,PrintPartitons[];
+        int NumOfPartitions, PartitionSize[], NumOfProcess, ProcessSize[], ans = 0,PrintPartitons[],choice=0,ExternalFrag[];
         boolean check[];
-        String PartitionName[], ProcessName[];
+        String PartitionName[], ProcessName[],NotAllocated[];
 
         System.out.println("Please enter Number of Partitions: ");
         NumOfPartitions = s.nextInt();
@@ -13,6 +13,8 @@ class FirstFit {
         PartitionSize = new int[NumOfPartitions];
         PartitionName = new String[NumOfPartitions];
         for (int i = 0; i < NumOfPartitions; i++) {
+            System.out.println("Please enter Name of Partition " + (i+1) + ": ");
+            PartitionName[i] = s.next();
             System.out.println("Please enter Size of Partition " + (i+1) + ": ");
             PartitionSize[i] = s.nextInt();
         }
@@ -24,17 +26,24 @@ class FirstFit {
         ProcessName = new String[NumOfProcess];
 
         for (int i = 0; i < NumOfProcess; i++) {
+            System.out.println("Please enter Name of Process " + (i+1) + ": ");
+            ProcessName[i] = s.next();
             System.out.println("Please enter Size of Process " + (i+1) + ": ");
             ProcessSize[i] = s.nextInt();
         }
 
         PrintPartitons = new int[NumOfPartitions];
         check=new boolean[NumOfPartitions];
-
+        NotAllocated= new String[NumOfProcess];
+        ExternalFrag=new int[NumOfPartitions];
         for (int i =0 ; i< NumOfPartitions;i++)
         {
             PrintPartitons[i]=0;
             check[i]=false;
+            if( i<NumOfProcess)
+            {
+                NotAllocated[i]=" ";
+            }
         }
 
         for (int i =0 ; i < NumOfProcess;i++)
@@ -47,16 +56,48 @@ class FirstFit {
                     PrintPartitons[j]=ProcessSize[i];
                     ans+=sub;
                     check[j]=true;
+                    NotAllocated[i]=ProcessName[i];
+                    ExternalFrag[j]=sub;
                     i++;
                     j=0;
+
+
                 }
             }
+
         }
 
         for (int i=0 ; i < NumOfPartitions;i++)
         {
-            System.out.println("Process "+(i+1)+ " "+PrintPartitons[i]);
+            if(PrintPartitons[i]==0)
+            {
+                System.out.println(PartitionName[i]+" "+PartitionSize[i]+ " "+"=> "+ "External Fragmentation");
+
+            }
+            else {
+                System.out.println(PartitionName[i]+" "+PartitionSize[i]+ " "+"=> "+PrintPartitons[i]);
+            }
         }
+
+        for (int i=0 ; i < NumOfProcess;i++)
+        {
+            if(i<NumOfProcess && NotAllocated[i]==" " )
+            {
+                System.out.println(ProcessName[i] +" "+ "Cannot be Allocated");
+            }
+        }
+
+
+//
+//        System.out.println("Do you want to compact? 1.yes 2.no\n");
+//        choice= s.nextInt();
+//        if(choice==1)
+//        {
+//
+//
+//        }
+//        else {return;}
+
 
 
     }
