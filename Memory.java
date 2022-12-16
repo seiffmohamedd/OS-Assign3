a5d8ae2094c4a70cb58aa9988665b811229a8f7d
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2022-12-16 18:32:10 +0200

    structure almost has been created

---
+++ Memory.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Memory {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter number of partition: ");
+        int n = reader.nextInt();
+        LinkedList <Integer> cars = new LinkedList<Integer>();
+
+    }
+}
